3f7d082bef849ac1da4b3b86c4604081891110dc
NishadGuha <54943334+NishadGuha@users.noreply.github.com>
2021-04-22 23:13:08 +0200

    Added cryptocurrency data in backend from CMC API

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const rp = require('request-promise');
+
+const requestOptions = {
+  method: 'GET',
+  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  qs: {
+    'start': '1',
+    'limit': '10',
+    'convert': 'USD'
+  },
+  headers: {
+    'X-CMC_PRO_API_KEY': '7c607d33-2cbd-455e-b2cb-3e2b7672fe20'
+  },
+  json: true,
+  gzip: true
+};
+
+app.get('/api/crypto', (req, res) => {
+
+    rp(requestOptions).then(response => {
+        res.json(response);
+        }).catch((err) => {
+        console.log('API call error:', err.message);
+        });
+})
 
 app.get('/api/customers', (req, res) => {
+
     const customers = [
         {id: 1, firstName: 'John', lastName: 'Doe'},
         {id: 2, firstName: 'Mike', lastName: 'Johnson'},
@@ -10,6 +36,7 @@ app.get('/api/customers', (req, res) => {
     ];
 
     res.json(customers);
+    console.log(req.baseUrl);
 })
 
 app.listen(port, () => console.log(`Server has started on port ${port}`));
