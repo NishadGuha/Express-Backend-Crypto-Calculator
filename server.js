5e7a13e1909d6fc90f249debca0cf792ccc22430
NishadGuha <54943334+NishadGuha@users.noreply.github.com>
2021-05-18 23:49:27 +0200

    successfully returning calculated result

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.get('/api/crypto', (req, res) => {
 })
 
 app.post('/result', (req, res) => {
-  console.log("I got a request!");
-  console.log(req.body);
+  // console.log("I got a request!");
+  // console.log(req.body);
 
   let historicPricePromise = new Promise((resolve, reject) => {
     let data = CoinGeckoClient.coins.fetchHistory('bitcoin', {
@@ -55,7 +55,6 @@ app.post('/result', (req, res) => {
 
   let currentPricePromise = new Promise((resolve, reject) => {
     let data =  CoinGeckoClient.coins.fetch('bitcoin', {
-      market_data: true
     });
     if(data != null) {
       resolve(data)
@@ -64,22 +63,24 @@ app.post('/result', (req, res) => {
     }
   })
 
-  historicPricePromise.then((message) => {
-    var historicPrice = message.data.market_data.current_price.usd;
-    var historicCryptoValue = message.data.market_data.current_price.usd/req.body.amount;
-
-    var currentPrice = currentPricePromise.then(function (currentPriceMessage) {
-      return currentPriceMessage.market_data.current_price.usd;
-    }).catch((err) => {
-      console.log("Error fetching current day price");
-    });
-
-    var currentFiatValue = historicCryptoValue * currentPrice;
-
-    console.log(`You had bought ${historicCryptoValue} BTC at ${historicPrice} and you would now have ${currentFiatValue} dollars!`);
+  currentPricePromise.then((currentPriceMessage) => {
+    var currentPrice =  currentPriceMessage.data.market_data.current_price.usd;
+    calculateResult(currentPrice);
   }).catch((err) => {
-    console.log(err)
-  })
-});
+    console.log("Error fetching current day price" + err);
+  });
 
+  function calculateResult(currentPrice) {
+    historicPricePromise.then((message) => {
+      var historicPrice = message.data.market_data.current_price.usd;
+      var historicCryptoValue = req.body.amount/message.data.market_data.current_price.usd;  
+      var currentFiatValue = historicCryptoValue * currentPrice;
+  
+      console.log(`You had bought ${historicCryptoValue} BTC at ${historicPrice} and you would now have ${currentFiatValue} dollars!`);
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+  
+});
 app.listen(port, () => console.log(`Server has started on port ${port}`));
